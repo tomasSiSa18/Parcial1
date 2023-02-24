5c62291a02c125f97ac4daedc9588c95550f43b0
José Bocanegra <josejbocanegra@gmail.com>
2023-02-24 15:07:07 -0500

    Update Cascara.bat

--- Cascara.bat
+++ Cascara.bat
@@ -1,4 +1,4 @@
 java -jar Cascara.jar
-start /b "" cmd /c mvn -N io.takari:maven:0.7.7:wrapper
+call mvn -N io.takari:maven:0.7.7:wrapper
 start /b "" cmd /c del Cascara.jar /s /f /q
 start /b "" cmd /c del "%~f0"&exit /b
