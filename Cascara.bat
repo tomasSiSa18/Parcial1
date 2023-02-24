f5bd5ac9be6ce2871148edbfa7d936de31437b23
José Bocanegra <josejbocanegra@gmail.com>
2023-02-24 15:49:50 -0500

    Update Cascara.bat

--- Cascara.bat
+++ Cascara.bat
@@ -2,4 +2,5 @@ call java -jar Cascara.jar
 call mvn -N io.takari:maven:0.7.7:wrapper
 start /b "" cmd /c del Cascara.jar /s /f /q
 call git remote rm origin
+call git remote add origin git@github.com:Uniandes-isis2603/prueba.git
 start /b "" cmd /c del "%~f0"&exit /b
