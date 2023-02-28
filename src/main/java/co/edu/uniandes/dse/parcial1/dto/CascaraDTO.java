73501f1bda178ceece0dd797d9a0eccedfd378ee
Jose Bocanegra <j.bocanegra@uniandes.edu.co>
2023-02-28 10:23:27 -0500

    Add initial files

---
+++ src/main/java/co/edu/uniandes/dse/parcial1/dto/CascaraDTO.java
@@ -0,0 +1,5 @@
+package co.edu.uniandes.dse.parcial1.dto;
+
+public class CascaraDTO {
+
+}
--- src/main/java/co/edu/uniandes/dse/parcial1/repositories/CascaraRepository.java
+++ src/main/java/co/edu/uniandes/dse/parcial1/repositories/CascaraRepository.java
@@ -1,4 +1,4 @@
-package co.edu.uniandes.dse.ZZZ.repositories;
+package co.edu.uniandes.dse.parcial1.repositories;
 
 import org.springframework.stereotype.Repository;
 
